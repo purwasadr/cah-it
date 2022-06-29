a4c743c3179e438a339266926e31369b9ef3cd5e
Purwa Sadr <purwasadr@gmail.com>
2022-06-29 17:09:18 +0700

    refactor: create proper card

--- components/card/card-post.tsx
+++ components/card/card-post.tsx
@@ -1,21 +1,26 @@
-interface Props {
+import { BACKEND_API } from 'libs/constans';
+import { Post } from 'models/post';
+import Image from 'next/image';
+import { getDateShort } from 'utils';
+
+interface Props extends Post {
     className?: string;
-    title?: string;
-    excerpt?: string;
-    category: string;
-    date?: string;
 }
 
-const CardPost = ({className = '', title, excerpt, category, date = ''}: Props) => {
+const CardPost = ({className = '', title, slug, image, excerpt, category, date = ''}: Props) => {
     return (
         <div
             className={`w-full h-full md:w-[260px] ${className}`}
         >
-            <h3 className="text-3xl">{title}</h3>
-            <p>{excerpt}</p>
-            <div>
+            <div className="relative aspect-[3/2] rounded-md overflow-hidden shadow-md">
+                <Image className="" src={`${BACKEND_API}${image}`} layout="fill" objectFit="cover" alt="Image"/>
+            </div>
+            <h2 className="mt-4 text-2xl font-medium">{title}</h2>
+            <p className="mt-2 text-slate-500">{excerpt}</p>
+            <div className="flex gap-2 mt-3 text-sm">
                 <p>{category}</p>
-                <p>{date}</p>
+                <span>•</span>
+                <p>{getDateShort(new Date(date))}</p>
             </div>
         </div>
     );
