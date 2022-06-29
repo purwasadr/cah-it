cbe7f4d2332d5c53cc7ddcab2fc1544abcd4f435
Purwa Sadr <purwasadr@gmail.com>
2022-06-29 09:49:47 +0700

    feat: add primary layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import Primary from '../components/layout/primary';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <Primary>
+            <Component {...pageProps} />
+        </Primary>
+    );
 }
 
-export default MyApp
+export default MyApp;
