1e43b87d78a5fc7b8534f648f3946600b31bb08b
Purwa Sadr <purwasadr@gmail.com>
2022-07-01 07:39:09 +0700

    refactor: add getPost method and PostItem type

--- models/post.ts
+++ models/post.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import qs from 'qs';
-import { toPosts } from 'utils/mapper';
+import { toPost, toPosts } from 'utils/mapper';
 import { BACKEND_API } from '../libs/constans';
+
 
 export interface Post {
     id: number;
@@ -9,7 +10,18 @@ export interface Post {
     slug?: string;
     image?: string;
     excerpt?: string;
-    category: string;
+    body?: string;
+    category?: string;
+    date?: string;
+}
+
+export interface PostItem {
+    id: number;
+    title?: string;
+    slug?: string;
+    image?: string;
+    excerpt?: string;
+    category?: string;
     date?: string;
 }
 
@@ -19,13 +31,28 @@ const getPosts = async () => {
     }, { encodeValuesOnly: true });
 
     const res = await axios.get(`${BACKEND_API}/api/posts?${query}`);
-    console.log('getPostsQuery:', query);
     
     return toPosts(res.data.data);
 }
 
-const ModelPost = {
-    getPosts
+const getPost = async (slug: string) => {
+    const qsQuery = qs.stringify({
+        filters: {
+            slug: {
+                $eq: slug
+            }
+        },
+        populate: ['image', 'category']
+    }, { encodeValuesOnly: true });
+
+    const res = await axios.get(`${BACKEND_API}/api/posts?${qsQuery}`);
+    
+    return toPost(res.data.data[0]);
 }
 
-export default ModelPost;
+const PostModel = {
+    getPosts,
+    getPost
+}
+
+export default PostModel;
