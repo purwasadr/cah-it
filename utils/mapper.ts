5fea1556f0c5d93d8b743a4282f1a587198e299c
Purwa Sadr <purwasadr@gmail.com>
2022-07-01 07:36:09 +0700

    refactor: add toPost mapper
    and also change type in toPosts mapper

--- utils/mapper.ts
+++ utils/mapper.ts
@@ -1,13 +1,26 @@
-import { Post } from 'models/post';
+import { Post, PostItem } from 'models/post';
 
 export const toPosts = (posts: any[]) => {
-    return posts.map((post): Post => ({
+    return posts.map((post): PostItem => ({
         id: post.id,
         title: post.attributes.title,
         slug: post.attributes.slug,
-        image: post.attributes.image.data.attributes.formats.medium.url,
+        image: post.attributes.image?.data?.attributes?.formats?.medium?.url,
         excerpt: post.attributes.excerpt,
         category: post.attributes.category.data.attributes.name,
         date: post.attributes.createdAt
     }));
+}
+
+export const toPost = (post: any): Post => {
+    return {
+        id: post.id,
+        title: post.attributes.title,
+        slug: post.attributes.slug,
+        image: post.attributes.image?.data?.attributes?.formats?.medium?.url,
+        excerpt: post.attributes?.excerpt,
+        body: post.attributes?.body,
+        category: post.attributes.category.data.attributes.name,
+        date: post.attributes.createdAt
+    };
 }
