8de0084330dc2869abd57016052a0ae1d7566571
Purwa Sadr <purwasadr@gmail.com>
2022-07-05 15:11:51 +0700

    change: change source data posts
    Change source posts data from ssr to client side with paging

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,47 @@
 import type {NextPage} from 'next';
 import Image from 'next/image';
-import { GetServerSideProps } from 'next';
-import ModelPost, { Post } from 'models/post';
 import CardPost from '@/components/card/card-post';
 import { getDateShort } from 'utils';
+import usePostPaging from 'libs/hooks/usePostPaging';
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+const Home: NextPage = () => {
+    const {isLoading, error, posts, lastPostElementRef} = usePostPaging();
 
-    try {
-        const posts = await ModelPost.getPosts()
-
-        return{
-            props:{
-                posts: {
-                    data: posts
-                }
-            }
-        } 
-    } catch (error) {
-        return {
-            props:{
-                posts: {
-                    error: 'error'
-                }
-            }
-        }
-    }
-}
-interface PageProps {
-    posts: { data?: Post[], error?: string }
-}
-
-const Home: NextPage<PageProps> = ({ posts }) => {
-    
     return (
         <>
-            <section className="flex gap-8">
-                <section className="w-2/3">
-                    <div className="relative rounded-lg h-[300px] shadow-md overflow-hidden">
+            <section className="flex flex-col md:flex-row gap-2">
+                <section className="w-full md:w-[60%] lg:w-2/3">
+                    <div className="relative rounded-lg aspect-[10/6] md:aspect-[20/9] shadow-md overflow-hidden">
                         <Image src="/img1.jpg" alt="Poster" layout="fill" objectFit="cover" />
                     </div>
                 </section>
-                <section className="flex flex-col justify-center w-1/3">
-                    <div className="">
-                    <h2 className="mt-4 text-2xl font-medium">Loem</h2>
+                <section className="flex flex-col justify-center w-full md:w-[40%] lg:w-1/3 pl-3">
+                    <h2 className="mt-3.5 text-2xl font-medium">Loem</h2>
                     <p className="mt-2 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, error.</p>
                     <div className="flex gap-2 mt-3 text-sm">
                         <p>Shounen</p>
                         <span>|</span>
-                        <p>{getDateShort(new Date('2002-8-2'))}</p>
+                        <p>{getDateShort('2002-8-2')}</p>
                     </div>
-                    </div>
+              
                 </section>
             </section>
-            <section className="mt-8">
+            <section className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
-                    posts.data?.map((post) => (<CardPost key={post.id} {...post}/>))
+                    posts.map((post, i) => posts.length !== i + 1 ?
+                    (
+                        <CardPost key={post.id} {...post}/>
+                    ) : (
+                        <CardPost key={post.id} {...post} inputRef={lastPostElementRef}/>
+                    ))
                 }
             </section>
+            {isLoading && <div className='flex justify-center mt-4'>Loading...</div>}
+            {!isLoading && error && <div className='flex justify-center mt-4'>Error get data</div>}
             <div className="h-[400px]"></div>
         </>
     );
 };
 
 export default Home;
+
