77e9546e00002197d5c29e621c047549c77eba3b
Purwa Sadr <purwasadr@gmail.com>
2022-07-05 15:12:30 +0700

    refactor: add toPostSearch mapper

--- utils/mapper.ts
+++ utils/mapper.ts
@@ -1,4 +1,4 @@
-import { Post, PostItem } from 'models/post';
+import { Post, PostItem, PostSearch } from 'models/post';
 
 export const toPosts = (posts: any[]) => {
     return posts.map((post): PostItem => ({
@@ -8,19 +8,28 @@ export const toPosts = (posts: any[]) => {
         image: post.attributes.image?.data?.attributes?.formats?.medium?.url,
         excerpt: post.attributes.excerpt,
         category: post.attributes.category.data.attributes.name,
-        date: post.attributes.createdAt
+        date: post?.attributes?.createdAt,
+        author: post?.attributes?.author?.data?.attributes?.username
     }));
 }
 
 export const toPost = (post: any): Post => {
     return {
+        id: post?.id,
+        title: post?.attributes?.title,
+        slug: post?.attributes?.slug,
+        image: post?.attributes?.image?.data?.attributes?.formats?.medium?.url,
+        excerpt: post?.attributes?.excerpt,
+        body: post?.attributes?.body,
+        category: post?.attributes?.category?.data?.attributes?.name,
+        date: post?.attributes?.createdAt
+    };
+}
+
+export const toPostSearch = (posts: any[]): PostSearch[] => {
+    return posts.map((post): PostSearch => ({
         id: post.id,
         title: post.attributes.title,
-        slug: post.attributes.slug,
-        image: post.attributes.image?.data?.attributes?.formats?.medium?.url,
-        excerpt: post.attributes?.excerpt,
-        body: post.attributes?.body,
-        category: post.attributes.category.data.attributes.name,
-        date: post.attributes.createdAt
-    };
+        slug: post?.attributes?.slug,
+    }));
 }
