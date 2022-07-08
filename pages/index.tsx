79cc78f03fd2d538a6fa2391ffd692a2d9ab5347
Purwa Sadr <purwasadr@gmail.com>
2022-07-08 22:01:59 +0700

    feat: add featured post fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,49 @@ import Image from 'next/image';
 import CardPost from '@/components/card/card-post';
 import { getDateShort } from 'utils';
 import usePostPaging from 'libs/hooks/usePostPaging';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import PostModel, { PostItem } from 'models/post';
+import Spinner from '@/components/spinner/spinner';
 
 const Home: NextPage = () => {
     const {isLoading, error, posts, lastPostElementRef} = usePostPaging();
+    const [featuredPost, setFeaturedPost] = useState<PostItem>();
+
+    useEffect(() => {
+        const req = async () => {
+            try {
+                const reqPost = await PostModel.getFeaturedPost();
+                setFeaturedPost(reqPost);
+            } catch (error) {
+                console.error(error);
+                
+            }
+        }
+        req()
+    }, [])
 
     return (
         <>
-            <section className="flex flex-col md:flex-row gap-2">
+            <section className="flex flex-col md:flex-row md:space-x-5">
                 <section className="w-full md:w-[60%] lg:w-2/3">
                     <div className="relative rounded-lg aspect-[10/6] md:aspect-[20/9] shadow-md overflow-hidden">
                         <Image src="/img1.jpg" alt="Poster" layout="fill" objectFit="cover" />
                     </div>
                 </section>
-                <section className="flex flex-col justify-center w-full md:w-[40%] lg:w-1/3 pl-3">
-                    <h2 className="mt-3.5 text-2xl font-medium">Loem</h2>
-                    <p className="mt-2 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, error.</p>
-                    <div className="flex gap-2 mt-3 text-sm">
-                        <p>Shounen</p>
-                        <span>|</span>
-                        <p>{getDateShort('2002-8-2')}</p>
+                <section className="flex flex-col justify-center w-full lg:w-1/3">
+                    <p className="pt-4 text-sky-700 tracking-wider uppercase text-sm font-medium">{featuredPost?.category}</p>
+                    <Link href={`/${encodeURIComponent(featuredPost?.slug ?? '')}`}>
+                        <a>
+                            <h2 className="pt-1 text-xl font-semibold">{featuredPost?.title}</h2>
+                        </a>
+                    </Link>
+                    <p className="pt-1 text-slate-500">{featuredPost?.excerpt}</p>
+                    <div className="flex items-center gap-2 pt-2 text-sm">
+                        <p>{featuredPost?.author}</p>
+                        <span className="text-xs">•</span>
+                        <p>{getDateShort(featuredPost?.date) ?? '-'}</p>
                     </div>
-              
                 </section>
             </section>
             <section className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -36,9 +58,8 @@ const Home: NextPage = () => {
                     ))
                 }
             </section>
-            {isLoading && <div className='flex justify-center mt-4'>Loading...</div>}
+            {isLoading && (<div className="flex justify-center items-center min-h-[300px]"><Spinner /></div>)}
             {!isLoading && error && <div className='flex justify-center mt-4'>Error get data</div>}
-            <div className="h-[400px]"></div>
         </>
     );
 };
