7493f65759d582b1cd114f44d242762f75a8d28a
Purwa Sadr <purwasadr@gmail.com>
2022-07-09 20:24:29 +0700

    refactor: add toPostItem and toCategories func

--- utils/mapper.ts
+++ utils/mapper.ts
@@ -1,16 +1,30 @@
+import { Category } from 'models/category';
 import { Post, PostItem, PostSearch } from 'models/post';
 
 export const toPosts = (posts: any[]) => {
     return posts.map((post): PostItem => ({
-        id: post.id,
-        title: post.attributes.title,
-        slug: post.attributes.slug,
-        image: post.attributes.image?.data?.attributes?.formats?.medium?.url,
-        excerpt: post.attributes.excerpt,
-        category: post.attributes.category.data.attributes.name,
+        id: post?.id,
+        title: post?.attributes?.title,
+        slug: post?.attributes?.slug,
+        image: post?.attributes?.image?.data?.attributes?.formats?.medium?.url,
+        excerpt: post?.attributes?.excerpt,
+        category: post?.attributes?.category?.data?.attributes?.name,
         date: post?.attributes?.createdAt,
         author: post?.attributes?.author?.data?.attributes?.username
     }));
+}
+
+export const toPostItem = (post: any): PostItem => {
+    return {
+        id: post?.id,
+        title: post?.attributes?.title,
+        slug: post?.attributes?.slug,
+        image: post?.attributes?.image?.data?.attributes?.formats?.medium?.url,
+        excerpt: post?.attributes?.excerpt,
+        category: post?.attributes?.category?.data?.attributes?.name,
+        date: post?.attributes?.createdAt,
+        author: post?.attributes?.author?.data?.attributes?.username
+    };
 }
 
 export const toPost = (post: any): Post => {
@@ -31,5 +45,13 @@ export const toPostSearch = (posts: any[]): PostSearch[] => {
         id: post.id,
         title: post.attributes.title,
         slug: post?.attributes?.slug,
+    }));
+}
+
+export const toCategories = (categories: any[]) => {
+    return categories.map((category): Category => ({
+        id: category?.id,
+        name: category?.attributes?.name,
+        slug: category?.attributes?.slug,
     }));
 }
