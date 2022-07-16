316c4762f6612435fe11c9e0a4ce8c35aa9b8b0e
Purwa Sadr <purwasadr@gmail.com>
2022-07-16 13:46:01 +0700

    refactor: add loading and error message

--- components/dialog/dialog-categories.tsx
+++ components/dialog/dialog-categories.tsx
@@ -1,6 +1,8 @@
 import { Dialog } from '@headlessui/react';
+import useResource from 'libs/hooks/useResource';
 import CategoryModel, { Category } from 'models/category';
 import { useEffect, useState } from 'react';
+import Spinner from '../spinner/spinner';
 
 interface Props {
     isDialogOpen: boolean;
@@ -8,30 +10,30 @@ interface Props {
 }
 
 const DialogCategories = ({isDialogOpen, setDialogOpen}: Props) => {
-    const [items, setItems] = useState<Array<Category>>([]);
-
-    useEffect(() => {
-        CategoryModel.getCategories()
-            .then((data) => {
-                setItems(data);
-            })
-            .catch((reason) => {
-                console.error(reason);
-            })
-    }, [])
+    const [items, isLoading, error] = useResource(CategoryModel.getCategories());
 
     return (
         <Dialog className="fixed inset-0 z-10" open={isDialogOpen} onClose={() => setDialogOpen(false)}>
             <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-10" />
-            <div className="flex justify-center items-center min-h-screen px-4">
+            <div className="flex justify-center items-center h-screen px-4">
                 <div className="w-full max-w-sm overflow-y-auto max-h-screen overflow-hidden transition-all transform bg-white shadow-xl rounded-2xl">
-                    <ul className="max-h-80 divide-y-[1px]">
-                        {items.map((item) => (
+                    {(!isLoading && !error) && (<ul className="max-h-80 divide-y-[1px]">
+                        {items?.map((item) => (
                             <li key={item.id}>
                                 <div className="py-3 px-4 font-medium hover:bg-slate-100 cursor-pointer" onClick={() => setDialogOpen(false)}>{item.name}</div>
                             </li>
                         ))}
-                    </ul>
+                    </ul>)}
+                    {!isLoading && error && ((
+                        <div className="flex justify-center items-center h-20 max-h-screen">
+                            <p>Error fetch categories</p>
+                        </div>
+                    ))}
+                    {isLoading && (
+                        <div className="flex justify-center items-center h-20 max-h-screen">
+                            <Spinner />
+                        </div>
+                    )}
                 </div>
             </div>
         </Dialog>
