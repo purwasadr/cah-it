159e7f05e852dd55ea3ce067f672d8434ba587c6
Purwa Sadr <purwasadr@gmail.com>
2022-07-16 13:46:50 +0700

    feat: add loading & error message in featuredPost

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,27 @@ import { useEffect, useState } from 'react';
 import PostModel, { PostItem } from 'models/post';
 import Spinner from '@/components/spinner/spinner';
 import { BACKEND_API } from 'libs/constants';
+import useResource from 'libs/hooks/useResource';
 
 const Home: NextPage = () => {
     const {isLoading, error, posts, lastPostElementRef} = usePostPaging();
-    const [featuredPost, setFeaturedPost] = useState<PostItem>();
-
-    useEffect(() => {
-        const req = async () => {
-            try {
-                const reqPost = await PostModel.getFeaturedPost();
-                setFeaturedPost(reqPost);
-            } catch (error) {
-                console.error(error);
-                
-            }
-        }
-        req()
-    }, [])
+    const [
+            featuredPost, 
+            isFeaturedPostLoading, 
+            errorFeaturedPost 
+        ] = useResource(PostModel.getFeaturedPost());
 
     return (
         <>
-            <section className="flex flex-col md:flex-row md:space-x-5">
+            {featuredPost
+                && 
+            (<section className="flex flex-col md:flex-row md:space-x-5">
                 <section className="w-full md:w-[60%] lg:w-2/3">
-                    <div className="relative rounded-lg aspect-[10/6] md:aspect-[20/9] shadow-md overflow-hidden">
+                    <div className="relative aspect-[10/6] md:aspect-auto md:h-[250px] lg:h-[300px] rounded-lg shadow-md overflow-hidden">
                        {featuredPost?.image && <Image src={`${BACKEND_API}${featuredPost?.image}`} alt="Poster" layout="fill" objectFit="cover" />}
                     </div>
                 </section>
-                <section className="flex flex-col justify-center w-full lg:w-1/3">
+                <section className="flex flex-col justify-center w-full md:w-[40%] lg:w-1/3">
                     <p className="pt-4 text-sky-700 tracking-wider uppercase text-sm font-medium">{featuredPost?.category}</p>
                     <Link href={`/${encodeURIComponent(featuredPost?.slug ?? '')}`}>
                         <a>
@@ -48,7 +42,10 @@ const Home: NextPage = () => {
                         <p>{getDateShort(featuredPost?.date) ?? '-'}</p>
                     </div>
                 </section>
-            </section>
+            </section>)}
+            {isFeaturedPostLoading && (<div className="flex justify-center items-center h-[300px]"><Spinner /></div>)}
+            {errorFeaturedPost && (<div className="flex justify-center items-center h-[300px]">Error fetch data</div>)}
+            
             <section className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
                     posts.map((post, i) => posts.length !== i + 1 ?
