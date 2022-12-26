6b2450043baf830b77b186e399fb9b86a8dd3953
Purwa Sadr <purwasadr@gmail.com>
2022-12-26 17:39:38 +0700

    remove : remove search dialog

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,6 @@ import PostModel, {PostSearch} from '@/models/post';
 import Link from 'next/link';
 import {ChangeEvent, forwardRef, useRef, useState} from 'react';
 import DialogCategories from '../dialog/dialog-categories';
-import SearchIcon from '../icon/search-icon';
 import NavbarLink from './navbar-link';
 
 interface PropsMenuLink {
@@ -31,35 +30,8 @@ const MenuLink = forwardRef<HTMLAnchorElement, PropsMenuLink>(
 MenuLink.displayName = 'MenuLink';
 
 const Navbar = () => {
-    const [isOpenDialog, setIsOpenDialog] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
-    const searchTimer = useRef<NodeJS.Timeout>();
-    const [selectedPerson, setSelectedPerson] = useState<PostSearch>();
-    const [searchResult, setSearchResult] = useState<Array<PostSearch>>([]);
     const [isDialogCategoriesOpen, setIsDialogCategoriesOpen] = useState(false);
 
-    const handleSearchQueryChange = async (
-        e: ChangeEvent<HTMLInputElement>
-    ) => {
-        setSearchQuery(e.target.value);
-
-        clearTimeout(searchTimer.current);
-
-        if (!e.target.value) {
-            setSearchResult([]);
-            return;
-        }
-
-        let timeout = setTimeout(() => {
-            PostModel.getPostSearch(e.target.value)
-                .then((value) => setSearchResult(value))
-                .catch((reason) => {
-                    setSearchResult([]);
-                });
-        }, 300);
-
-        searchTimer.current = timeout;
-    };
 
     return (
         <nav className="sticky top-0 z-10 px-2 sm:px-4 py-2.5 border-b border-gray-200 bg-white dark:bg-gray-800">
@@ -67,7 +39,7 @@ const Navbar = () => {
                 <div className="flex items-center w-auto">
                     <Link href="/" className="flex items-center">
                         <span className="text-xl font-bold text-slate-900 whitespace-nowrap dark:text-white">
-                            Ngeblog
+                            CahIT
                         </span>
                     </Link>
                 </div>
@@ -90,56 +62,6 @@ const Navbar = () => {
                             />
                         </li>
                     </ul>
-                </div>
-                <div className="border-l ml-auto md:ml-4 pl-4">
-                    <div
-                        className="cursor-pointer"
-                        onClick={() => setIsOpenDialog(true)}
-                    >
-                        <SearchIcon className="w-4 h-4 text-slate-700" />
-                    </div>
-                    <Dialog
-                        className="fixed inset-0 z-10"
-                        open={isOpenDialog}
-                        onClose={() => setIsOpenDialog(false)}
-                    >
-                        <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-10" />
-                        <div className="flex justify-center items-center min-h-screen px-4">
-                            <div className="w-full max-w-lg overflow-hidden transition-all transform bg-white shadow-xl rounded-2xl">
-                                <Combobox
-                                    value={selectedPerson}
-                                    onChange={() => setIsOpenDialog(false)}
-                                >
-                                    <div className="flex items-center gap-2 px-4 border-b-[1px]">
-                                        <SearchIcon className="h-4 w-4 text-slate-500" />
-                                        <Combobox.Input
-                                            className="w-full h-12 border-0  py-2 pl-3 pr-10 text-sm text-gray-900 focus:ring-0"
-                                            onChange={handleSearchQueryChange}
-                                        />
-                                    </div>
-                                    <div className="overflow-y-auto max-h-80">
-                                        <Combobox.Options>
-                                            {searchResult.map((post) => (
-                                                <Link
-                                                    key={post.id}
-                                                    href={`/${post.slug}`}
-                                                >
-                                                    <a>
-                                                        <Combobox.Option
-                                                            className="py-3 px-4 font-medium hover:bg-slate-100"
-                                                            value={post}
-                                                        >
-                                                            {post.title}
-                                                        </Combobox.Option>
-                                                    </a>
-                                                </Link>
-                                            ))}
-                                        </Combobox.Options>
-                                    </div>
-                                </Combobox>
-                            </div>
-                        </div>
-                    </Dialog>
                 </div>
                 <div className="md:hidden relative">
                     <Menu as="div">
