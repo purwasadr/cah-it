9dac074dd447456727e9256dac3f6694fc513fcd
Purwa Sadr <purwasadr@gmail.com>
2022-12-26 17:42:46 +0700

    refactor(discover): rearrange code

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -8,12 +8,19 @@ import usePaging from '@/hooks/usePaging';
 import {PostItem} from '@/models/post';
 import {BACKEND_API} from '@/libs/constants';
 import qs from 'qs';
-import { toPosts } from '@/utils/mapper';
+import {toPosts} from '@/utils/mapper';
 import Spinner from '@/components/spinner/spinner';
 import clsxm from '@/libs/clsxm';
 
-const CardPosts = ({className, query}: {className?: string, query: string }) => {
-    const {isLoading, error, items:posts, hasMore, loadMore} = usePaging(
+const CardPosts = ({className, query}: {className?: string; query: string}) => {
+    const {
+        isLoading,
+        error,
+        items: posts,
+        hasMore,
+        loadMore,
+        tryAgain
+    } = usePaging(
         (pageNum: number, query: string) => searchPostsUrl(query, pageNum),
         query,
         (res) => ({
@@ -35,6 +42,7 @@ const CardPosts = ({className, query}: {className?: string, query: string }) => 
                     page: pageNum,
                     pageSize: 5,
                 },
+                populate: ['image', 'category', 'author']
             },
             {encodeValuesOnly: true}
         );
@@ -44,34 +52,46 @@ const CardPosts = ({className, query}: {className?: string, query: string }) => 
     return (
         <section className={clsxm(className)}>
             <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {
-                    posts.map((post, i) => <CardPost key={post.id} {...post}/>)
-                }
+                {posts.map((post, i) => (
+                    <CardPost key={post.id} {...post} />
+                ))}
             </div>
             <div className="flex justify-center mt-4">
-                {hasMore && !isLoading ? <Button onClick={() => loadMore()}>Load More</Button> : isLoading ? <Spinner /> : null}
+                {hasMore && !isLoading && !error ? (
+                    <Button onClick={() => loadMore()}>Load More</Button>
+                ) : isLoading ? (
+                    <Spinner />
+                ) : error ? (
+                    <div className="flex items-center">
+                        <div className='mr-4'>Error fetch data</div>
+                        <Button onClick={() => tryAgain()}>Try Again</Button>
+                    </div>
+                ) : null}
             </div>
         </section>
-        
-    )
+    );
 };
 
 const Discover: NextPage = () => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [searchInput, setSearchInput] = useState('');
 
     return (
         <>
-            <div className="flex">
-                <Input
-                    className="rounded-r-none"
-                    value={searchQuery}
-                    handleChange={(e) => setSearchQuery(e.target.value)}
-                    id="search"
-                    name="search"
-                />
-                <Button className="rounded-l-none">Search</Button>
+            <div className="flex justify-center">
+                <form className="flex w-4/5" onSubmit={(e) => e.preventDefault()}>
+                    <Input
+                        className="rounded-r-none"
+                        value={searchInput}
+                        handleChange={(e) => setSearchInput(e.target.value)}
+                        id="search"
+                        name="search"
+                    />
+                    <Button className="rounded-l-none" onClick={() => setSearchQuery(searchInput)}>Search</Button>
+                </form>
             </div>
-            <CardPosts query={searchQuery}/>
+          
+            <CardPosts className="mt-2" query={searchQuery} />
         </>
     );
 };
